d90b3138e2aceb21ad40adef48e4d78fb67d8e84
Ryan Pitts <ryanpitts@protonmail.com>
2019-01-09 22:46:13 -0400

    changed color of name

--- src/containers/Welcome.jsx
+++ src/containers/Welcome.jsx
@@ -6,7 +6,7 @@ const Welcome = () => (
   <div className="background rounded-0 text-center d-flex align-items-center justify-content-center">
     <FadeIn>
       <h1 className="display-3 text-white font-weight-bold mb-4">Hello, I am</h1>
-      <h2 className="display-4 text-black font-weight-bold my-4">Ryan Pitts</h2>
+      <h2 className="display-4 font-weight-bold my-4 name">Ryan Pitts</h2>
       <h3 className="display-5 text-white font-weight-bold my-4">Full Stack Web Developer from Nova Scotia, Canada</h3>
       <Button id="hireme" className="mt-2 font-weight-bold">HIRE ME  ⮞  </Button>
     </FadeIn>
