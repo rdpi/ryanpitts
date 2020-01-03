fd9b9dbe9ade8945342be131007cbe3fafdb7401
Ryan Pitts <ryanpitts@protonmail.com>
2020-01-03 02:04:40 -0400

    update linkedin url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => (
       <i className="fab fa-github" />
       <span>GitHub</span>
     </a>
-    <a href="https://www.linkedin.com/in/ryan-pitts-a9390b8a/" target="_blank" rel="noopener noreferrer" className="d-flex flex-column text-center footercontent mx-4">
+    <a href="https://www.linkedin.com/in/ryan-d-pitts" target="_blank" rel="noopener noreferrer" className="d-flex flex-column text-center footercontent mx-4">
       <i className="fab fa-linkedin" />
       <span>Linkedin</span>
     </a>
